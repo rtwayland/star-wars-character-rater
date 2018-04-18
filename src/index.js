8017500bb953668fd2da3ed07c7a4c5bdec9fb3c
Raleigh Wayland <rtwayland@gmail.com>
2018-04-18 17:39:11 -0600

    removing code for presentation purposes

--- src/index.js
+++ src/index.js
@@ -3,11 +3,8 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import 'semantic-ui-css/semantic.min.css';
 import App from './App';
-import registerServiceWorker from './registerServiceWorker';
 
 ReactDOM.render(<App />, document.getElementById('root'));
-
-registerServiceWorker();
 
 if (module.hot) {
 	module.hot.accept();
