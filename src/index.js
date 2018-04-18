1064da203767b7436ce22f2f8e052ba97c3bf151
Raleigh Wayland <rtwayland@gmail.com>
2018-04-18 17:33:47 -0600

    Spacing

--- src/index.js
+++ src/index.js
@@ -6,6 +6,7 @@ import App from './App';
 import registerServiceWorker from './registerServiceWorker';
 
 ReactDOM.render(<App />, document.getElementById('root'));
+
 registerServiceWorker();
 
 if (module.hot) {
