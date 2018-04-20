5363a2dcab26e846f5b6d5333733821fe4d7f000
Raleigh Wayland <rtwayland@gmail.com>
2018-04-20 12:45:00 -0600

    Adding in boilerplate for presentation

--- src/components/Nav.js
+++ src/components/Nav.js
@@ -4,6 +4,14 @@ import Search from './Search';
 
 const Nav = props => (
 	<nav className="nav-content">
+		<div className="nav-links">
+			<a className="nav-link" href="#">
+				Home
+			</a>
+			<a className="nav-link" href="#">
+				Leader Board
+			</a>
+		</div>
 		<Leader characters={props.characters} />
 		<Search searchValue={props.searchValue} searchCharacters={props.searchCharacters} />
 	</nav>
