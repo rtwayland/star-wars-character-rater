fb4ffb4f1ec63a0da24bd07160148aebfa5c3e02
Raleigh Wayland <rtwayland@gmail.com>
2018-04-20 16:22:05 -0600

    A small example of using state

--- src/App.js
+++ src/App.js
@@ -1,7 +1,30 @@
+import React, { Component } from 'react';
 
-
-const App = () => {
-	return 'Hello World';
+class App extends Component {
+	state = {
+		points: 0
+	};
+	render() {
+		return (
+			<div id="center">
+				<p>Points: 0</p>
+				<div className="buttons">
+					<button
+						className="ui basic green button"
+						onClick={null}
+					>
+						Up
+					</button>
+					<button
+						className="ui basic yellow button"
+						onClick={null}
+					>
+						Down
+					</button>
+				</div>
+			</div>
+		);
+	}
 }
 
 export default App;
