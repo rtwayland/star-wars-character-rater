d226f3a13bea7d6ad9ccb42a0c0ddf59cbae046b
Raleigh Wayland <rtwayland@gmail.com>
2018-04-22 23:32:55 -0600

    Adding get leaders function for time reasons

--- src/components/LeaderBoard.js
+++ src/components/LeaderBoard.js
@@ -1,6 +1,12 @@
 import React from 'react';
 import Character from './Character';
 
+function getLeaders(characters) {
+	return Object.values(characters)
+		.filter(character => character.upVotes > 0 || character.downVotes > 0)
+		.sort((a, b) => b.upVotes - b.downVotes - (a.upVotes - a.downVotes));
+}
+
 const LeaderBoard = props => {
 	return (
 		<div className="leader-board-container">
